73cb257cacaccc873bdcf829341e669084982f05
RawanHY <rawanhajyihya2000@gmail.com>
2024-09-18 12:11:25 +0300

    .

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../features/login/data/repos/login_repo.dart';
+import '../../features/login/logic/cubit/login_cubit.dart';
+import '../networking/api_service.dart';
+import '../networking/dio_factory.dart';
+
+final getIt = GetIt.instance;
+
+void setupGetIt() {
+  // Dio & ApiService
+  Dio dio = DioFactory.getDio();
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+
+  // login
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+}
